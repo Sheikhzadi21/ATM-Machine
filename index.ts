77d4d9dc24e0dde08008ce3ed93967bed660f99d
Alishba sheikh <alishbakhalidshaikh@gmail.com>
2024-04-24 13:50:11 -0700

    ATM machine Project

---
+++ index.ts
@@ -0,0 +1,55 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+// Initialize user balance and pin code//
+let myBalance = 5000;
+let myPin = 1234;
+
+//Print welcome message//
+console.log("Welcome to code with Alishba - ATM Machine");
+
+let pinAnswer = await inquirer.prompt([
+    {
+        name: "pin",
+        type: "number",
+        message: "Enter your Pincode:"
+    }
+])
+if(pinAnswer.pin === myPin){
+    console.log("Pin is correct, Login Succcessfully"); 
+// console.log(`Current Account balance is  ${myBalance}`)
+ let operationAns = await inquirer.prompt([
+    {
+        name: "operation",
+        type: "list",
+        message: "Select an operation",
+        choices: ["Withdraw Amount", "Check Balance"]
+    }
+ ])
+
+ if(operationAns.operation === "Withdraw Amount"){
+let amountAns = await inquirer.prompt([
+    {
+        name: "amount",
+        type: "number",
+        message: "Enter the amount to withdraw:"
+    }
+])
+
+  if(amountAns.amount > myBalance){
+    console.log("Insufficient Balance");
+  }
+  else{
+    myBalance -= amountAns.amount;
+    console.log(`${amountAns.amount} Withdraw Succcessfully`);
+    console.log(`Your Remaining Balance is: ${myBalance}`);
+  }
+ }
+ else if (operationAns.operation === "Check Balance"){
+    console.log(`Your Account Balance is: ${myBalance}`)
+ }
+}
+else{
+    console.log("Pin is incorrect, Try Again!");
+}
